fbf179d312e0cc38ed3cdae1546f689364a463c4
mohamed shkrui <mohamedshkry188@gmail.com>
2018-05-21 18:22:35 +0200

    files

---
+++ test.js
@@ -0,0 +1,77 @@
+var 
+	x="moahmed",
+	y='welcom to oxford',
+	z=30,
+	m=20,
+	name;
+// document.write("<p>welcom to my website</p>");
+
+// document.getElementById('demo').innerHTML= x;
+// name =prompt("ples neter name")
+// if (name==="heba"){
+// 	document.getElementById('day').innerHTML="Hoppy barth day"
+// } else {
+// 		document.getElementById('day').innerHTML="sorry hiss page not you"
+
+// }
+// window.onclick=function howTo(name) {
+// 	" user strict";
+// 	name =prompt("ples neter name")
+// 	if (name==="heba"){
+// 		document.getElementById('day').innerHTML="Hoppy barth day"
+// 	} else {
+// 			document.getElementById('day').innerHTML="sorry hiss page not you"
+
+// 	}
+// }
+// howTo();
+// function andTowNaber(a,b){
+// 	a=2;
+// 	b=3;
+// 	return a+b;
+// }
+// document.getElementById("demo").innerHTML=andTowNaber();
+
+
+var alpha =["mohame","shukry","abdalhamed","mohamud"];
+var alpha1=["hhh","jjjj","rrr"]
+var alpha2=alpha.indexOf("mohame")
+console.log(alpha2)
+//alpha.unshift("samy")
+alpha.pop("")
+
+
+
+/*
+splice add or hide item
+pop romav last item on array
+shift romov first item 
+sort sort item on array a to z 
+reverse sort item on array z to a
+var varbal name=ArrayNmae.indexof("name of search")
+*/
+
+ // document.getElementById("demo1").innerHTML=(alpha);
+ //  // document.getElementById("demo1").innerHTML=(y.length);
+ // document.getElementById("demo2").innerHTML=(alpha2);
+ //  document.getElementById("demo3").innerHTML=(alpha.concat(alpha1));
+
+
+
+
+
+
+
+
+  var img= "photo/1.jpeg",
+  	  img1="photo/2.jpeg",
+  	  img2="photo/3.jpeg",	
+  	  img3="photo/img7.jpeg",	
+  	  img4="photo/img2.jpeg";	
+ document.getElementById("demo3").src =img1;
+ document.getElementById("demo4").src =img2;
+ document.getElementById("demo5").src =img3;
+ document.getElementById("demo6").src =img4;
+ document.getElementById("demo3").src =img;
+ document.getElementById("demo3").src =img;
+
